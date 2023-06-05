8229b7ca47bc49426d8b99d04c1295c05e7ead73
valan <valanjudu@gmail.com>
2023-06-06 01:43:34 +0530

    changes applied

--- fileHandling.py
+++ fileHandling.py
@@ -1,3 +1,16 @@
 file_name = input("Enter a filename or path to read a file : ")
-f = open(file_name,'r')
-print(f.read())
+f = open(file_name, 'r')
+i = 0
+line = f.readline()
+while line:
+    line = f.readline()
+    print(line, end=',')
+    print(line.split(','))
+
+    if i % 1000 == 0:
+        op = print("Read more? [n]")
+        if op == 'n':
+            print('exiting')
+            break
+    i = i + 1
+
