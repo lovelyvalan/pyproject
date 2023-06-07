0c17375bce022e2bf807e9a5d9d8ffba834f9eb0
valan <valanjudu@gmail.com>
2023-06-08 01:04:32 +0530

    changes applied

--- writtenFile.py
+++ writtenFile.py
@@ -1,22 +1,24 @@
 file_name = input("Enter tehe file name : ")
 f = open(file_name, 'w',newline='')
 
-i = 0
+i = 1
 while True:
-    s = f.writelines(str(input(':write anything here . type stop to exit !').split(',')))
-
-    if i % 100 == 0:
-         op = input("Write more ? [y/n]")
-         if op == 'n':
+    s = str(input('write anything here . type stop to exit ! \n'))
+    f.writelines(s)
+    if s == 'stop':
+        break
+    if i % 10 == 0:
+        op = input("Write more ? [y/n]")
+        if op == 'n':
             break
-         elif op == 'y':
-             op
-         else:
-             while True:
-                 if op not in ['y','n'] and op.isalpha():
-                     print('invalid input please enter y or n !')
-                 else:
-                     break
+        elif op == 'y':
+            op
+        else:
+            while True:
+             if op not in ['y','n'] and op.isalpha():
+                print('invalid input please enter y or n !')
+             else:
+                 break
 
     i = i + 1
 
