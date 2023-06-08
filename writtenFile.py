ec89d448804d93c38c385ddf93f5aa089f742b6e
valan <valanjudu@gmail.com>
2023-06-08 20:41:01 +0530

    changes applied

--- writtenFile.py
+++ writtenFile.py
@@ -4,7 +4,7 @@ f = open(file_name, 'w',newline='')
 i = 1
 while True:
     s = str(input('write anything here . type stop to exit ! \n'))
-    f.writelines(s)
+    f.writelines(s+'\n')   # + and \n for write a line by line commands
     if s == 'stop':
         break
     if i % 10 == 0:
@@ -21,4 +21,16 @@ while True:
                  break
 
     i = i + 1
+
+
+fileName=input("enter the file nam eto append : ")
+
+while True:
+    ui=input("Enter line to append : q to quit : ")
+    if ui == 'q':
+        break
+    with open(fileName,'a') as f:
+        f.writelines(ui + '\n')
+    f.close()
+
 
